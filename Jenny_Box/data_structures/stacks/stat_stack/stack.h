2f91584f8e99274d87dac2ac129dcb294030e0ec
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-13 23:13:59 +0100

    Added a header file that contains all global variable declarations, function declarations, and more

---
+++ Jenny_Box/data_structures/stacks/stat_stack/stack.h
@@ -0,0 +1,39 @@
+#ifndef STACK_H
+#define STACK_H
+#define CAPACITY 256
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+
+/**
+ * stat_stack_int - Stack struct, abstracted data structure for stack
+ * stack_arr: Stack Array member of Stack Struct
+ * size: Size of stack at any given time
+ */
+
+typedef struct stat_stack_int
+{
+	int stack_arr[CAPACITY];
+	int size;
+} stat_stack_int;
+
+/**
+ * Global Variables 
+ */
+int top = -1;
+stat_stack_int = {[], 0};
+
+/**
+ * Stack OPerations
+ */
+void push(int data);
+int pop(void);
+int pop_at_index(size_t index);
+int peek(void);
+void display(stat_stack_int stack_inst);
+stat_stack_int rev_stack(stat_stack_int stack_inst);
+
+
+#endif
